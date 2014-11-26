1cc16e2241763eb30e6dccb967ea0516d4848a7b
saasbook <saasbook@saasbook.(none)>
2014-11-25 22:02:21 -0400

    Add routes and migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Myrottenpotatoes::Application.routes.draw do
+  resources :movies
+  root :to => redirect('/movies')
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
